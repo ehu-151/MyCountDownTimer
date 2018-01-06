c3530120808d6fe7c992203c5cb175f28e11467c
ehu <fehuehuf@gmail.com>
2018-01-06 16:28:59 +0900

    タイマー終了音を鳴らす。 画面を常にONにする

--- app/src/main/java/com/example/ehu/mycountdowntimer/MainActivity.java
+++ app/src/main/java/com/example/ehu/mycountdowntimer/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ehu.mycountdowntimer;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     TextView mTimerText;
     MyCountDownTimer mTimer;
     FloatingActionButton mFab;
+    SoundPool mSoundPool;
+    int mSoundResId;
 
     public class MyCountDownTimer extends CountDownTimer {
         public boolean isRunning = false;
@@ -32,7 +36,22 @@ public class MainActivity extends AppCompatActivity {
         public void onFinish() {
             //Timer終了時に「0:00」にセット
             mTimerText.setText("0:00");
+            //終了音をならす
+            mSoundPool.play(mSoundResId, 1.0f, 1.0f, 0, 0, 1.0f);
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSoundPool.release();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSoundPool = new SoundPool(2, AudioManager.STREAM_MUSIC, 0);
+        mSoundResId=mSoundPool.load(this,R.raw.bellsound,1);
     }
 
     @Override
@@ -43,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         //タイマーの初期設定
         mTimerText = (TextView) findViewById(R.id.timer_text);
         mTimerText.setText("3:00");
-        mTimer = new MyCountDownTimer(3 * 60 * 1000, 100);
+        mTimer = new MyCountDownTimer(1 * 3 * 1000, 100);
 
         //再生・停止ボタンの挙動
         mFab = (FloatingActionButton) findViewById(R.id.play_stop);
@@ -61,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
     }
 
 
